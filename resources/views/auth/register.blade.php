c4a2e8fdda4f396fca50425bb697e32641d1d95b
wibowo-fauzan <fauzankirana16@gmail.com>
2024-02-21 10:22:56 +0700

    laravel_ticket_ammar

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+@section('title', 'Login')
+@section('content')
+<div class="" style="margin-top: 20%;">
+    <div class="row card">
+        <div class="col-12">
+            <div class="p-5">
+                <h1 class="h4 text-gray-900 mb-4">Buat Akun!</h1>
+                <a href="/Ticket" class="text-start text-primary" style="text-align: start;">
+                    <p class="text-start"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
+                            viewBox="0 0 24 24">
+                            <g fill="none">
+                                <path
+                                    d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
+                                <path fill="currentColor"
+                                    d="M5.5 6a1.5 1.5 0 1 0-3 0v12a1.5 1.5 0 0 0 3 0zm7.556 11.303a1.5 1.5 0 0 0 0-2.121L11.374 13.5H20a1.5 1.5 0 0 0 0-3h-8.626l1.682-1.682a1.5 1.5 0 1 0-2.121-2.122L6.692 10.94a1.5 1.5 0 0 0 0 2.121l4.243 4.243a1.5 1.5 0 0 0 2.121 0Z" />
+                            </g>
+                        </svg> Back Home</p>
+                </a>
+                <form method="POST" action="{{ route('register') }}" class="user">
+                    @csrf
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-user @error('name') is-invalid @enderror"
+                            name="name" required autocomplete="off" autofocus placeholder="Nama User">
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <input type="text"
+                            class="form-control form-control-user @error('username') is-invalid @enderror"
+                            name="username" required autocomplete="off" placeholder="Username">
+                        @error('username')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <input type="password"
+                            class="form-control form-control-user @error('password') is-invalid @enderror"
+                            name="password" required placeholder="Password">
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control form-control-user" name="password_confirmation"
+                            required placeholder="Confirm Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-user btn-block mt-4">
+                        {{ __('Register') }}
+                    </button>
+                </form>
+                <hr>
+                <a href="{{ route('login') }}" class="btn btn-secondary btn-user btn-block"
+                    style="border-radius: 20px;">
+                    Sudah Punya Account!
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+<script>
+    $("body").addClass("bg-gradient-primary");
+
+</script>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home - Ticket Pesawat</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Arizonia&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+
+
+
+    <div class="hero-wrap js-fullheight" style="background-image: url('images/bg_5.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text js-fullheight align-items-center" data-scrollax-parent="true">
+                <div class="col-md-7 ftco-animate">
+                    <h1 class="mb-4">Selamat Datang di Pemesanan Tiket Pesawat</h1>
+                    <p class="caps">Aplikasi berbasis website untuk pemesanan tiket pesawat</p>
+                    <a href="/login" class="btn btn-primary">Pesan Sekarang!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center pb-4">
+                <div class="col-md-12 heading-section text-center ftco-animate">
+                    <span class="subheading">Destination</span>
+                    <h2 class="mb-4">Top Destination</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 ftco-animate">
+                    <div class="project-wrap">
+                        <a class="img" style="background-image: url(images/destination-1.jpg);">
+                        </a>
+                        <div class="text p-4">
+                            <h3><a>Keraton Yogyakarta</a></h3>
+                            <p class="location"><span class="fa fa-map-marker"></span> Jl. Rotowijayan Blok No. 1,
+                                Panembahan, Kecamatan Kraton, Kota Yogyakarta, Daerah Istimewa Yogyakarta</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <div class="project-wrap">
+                        <a class="img" style="background-image: url(images/destination-2.jpg);">
+                        </a>
+                        <div class="text p-4">
+                            <h3><a>Tangkuban Perahu</a></h3>
+                            <p class="location"><span class="fa fa-map-marker"></span> Cikahuripan, Kec. Lembang,
+                                Kabupaten
+                                Bandung Barat, Jawa Barat</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <div class="project-wrap">
+                        <a class="img" style="background-image: url(images/destination-3.jpg);">
+                        </a>
+                        <div class="text p-4">
+                            <h3><a>Monumen Nasional</a></h3>
+                            <p class="location"><span class="fa fa-map-marker"></span> Gambir, Kecamatan Gambir, Kota
+                                Jakarta Pusat, Daerah Khusus Ibukota Jakarta</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 ftco-animate">
+                    <div class="project-wrap">
+                        <a class="img" style="background-image: url(images/destination-4.jpg);">
+                        </a>
+                        <div class="text p-4">
+                            <h3><a>Lawang Sewu</a></h3>
+                            <p class="location"><span class="fa fa-map-marker"></span> Jl. Pemuda, Sekayu, Kec. Semarang
+                                Tengah, Kota Semarang, Jawa Tengah</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <div class="project-wrap">
+                        <a class="img" style="background-image: url(images/destination-5.jpg);">
+                        </a>
+                        <div class="text p-4">
+                            <h3><a>Tanah Lot</a></h3>
+                            <p class="location"><span class="fa fa-map-marker"></span> Beraban, Kec. Kediri, Kabupaten
+                                Tabanan, Bali</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <div class="project-wrap">
+                        <a class="img" style="background-image: url(images/destination-6.jpg);">
+                        </a>
+                        <div class="text p-4">
+                            <h3><a>Museum Angkut</a></h3>
+                            <p class="location"><span class="fa fa-map-marker"></span> Jl. Terusan Sultan Agung No.2,
+                                Ngaglik, Kec. Batu, Kota Batu, Jawa Timur</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="ftco-footer bg-bottom ftco-no-pt" style="background-image: url(images/bg_3.jpg);">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md pt-5">
+                    <div class="ftco-footer-widget pt-md-5 mb-4">
+                        <h2 class="ftco-heading-2">About</h2>
+                        <p>Kami dengan bangga mempersembahkan layanan pemesanan tiket pesawat yang nyaman dan andal,
+                            mengambil nama dari salah satu bandara tersibuk di Indonesia, Bandara Internasional
+                            Soekarno-Hatta. Sebagai pilihan utama untuk perjalanan udara Anda, kami berkomitmen untuk
+                            menyediakan pengalaman pemesanan yang mudah, cepat, dan aman.</p>
+                        <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                            <li class="ftco-animate"><a href="https://twitter.com/ConfidoTM" target="_blank"><span
+                                        class="fa fa-twitter"></span></a></li>
+                            <li class="ftco-animate"><a href="https://www.instagram.com/confidotm/?hl=id"
+                                    target="_blank"><span class="fa fa-facebook"></span></a></li>
+                            <li class="ftco-animate"><a href="#" target="_blank"><span
+                                        class="fa fa-instagram"></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md pt-5 border-left">
+                    <div class="ftco-footer-widget pt-md-5 mb-4">
+                        <h2 class="ftco-heading-2">Maskapai</h2>
+                        <ul class="list-unstyled">
+                            <li><a class="py-2 d-block">Garuda Indonesia</a></li>
+                            <li><a class="py-2 d-block">Citilink</a></li>
+                            <li><a class="py-2 d-block">Batik Air</a></li>
+                            <li><a class="py-2 d-block">Lion Air</a></li>
+                            <li><a class="py-2 d-block">Sriwijaya Air</a></li>
+                            <li><a class="py-2 d-block">Air Asia</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md pt-5 border-left">
+                    <div class="ftco-footer-widget pt-md-5 mb-4">
+                        <h2 class="ftco-heading-2">Have a Questions?</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><span class="icon fa fa-map-marker"></span><span class="text">South Fire Station 1,
+                                        Soekarno-Hatta International Airport, RT.001/RW.010, Pajang, Kec. Benda, Kota
+                                        Tangerang, Banten 15126</span></li>
+                                <li><a href="https://wa.me/6281384002161?text=Ingin%20Melaporkan%20Masalah"><span
+                                            class="icon fa fa-whatsapp"></span><span class="text">+62 877-4863-2717
+                                            Admin 1</span></a></li>
+                                <li><a href="https://wa.me/6288219983235?text=Ingin%20Melaporkan%20Masalah"><span
+                                            class="icon fa fa-whatsapp"></span><span class="text">+62 877-2177-8320
+                                            Admin 2</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Tiket Pesawat Soekarno-Hatta &copy; By Ammar
+                        <script>
+                            document.write(new Date().getFullYear());
+
+                        </script>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" /> <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /> </svg>
+    </div>
+
+    {{-- script js --}}
+
+    <script src="{{ asset('js/prices.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+    <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.animateNumber.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
+    <script src="{{ asset('js/scrollax.min.js') }}"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
+    </script>
+    <script src="{{ asset('js/google-map.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js">
+    </script>
+    <script>
+        $(document).ready(function () {
+            $('#table_id').DataTable();
+        });
+
+    </script>
+</body>
+
+</html>
